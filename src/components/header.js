73d317f6ba5de4956fcf45d229dde849b4bb9772
Dan <github@danp.dev>
2021-01-24 11:21:54 +1030

    initial

--- src/components/header.js
+++ src/components/header.js
@@ -15,9 +15,8 @@ return(
   
     <div id="navbarBasicExample" className="navbar-menu">
       <div className="menu-is-fullwidth is-uppercase is-size-5 has-text-weight-bold">
-        <a className="navbar-item">
-          Home
-        </a>
+      
+      <Link className="navbar-item" to="/">Home</Link>
   
         <a className="navbar-item">
           About
@@ -35,16 +34,15 @@ return(
         <div className="navbar-end">
           <div className="navbar-item">
             <div className="buttons">
-              <a className="button is-primary">
-                <strong>BOOK A TOUR</strong>
-              </a>
+            <Link className="button is-primary" to="/book-a-tour/">Book A Tour</Link>
 
-              <button className="button is-primary">
+              
+              <a className="button is-primary" href="tel:08 85222079">
                 <span className="icon">
                   <i className="fas fa-phone"></i>
                 </span>
                 <strong>Call Us</strong>
-              </button>
+              </a>
             </div>
           </div>
         </div>
