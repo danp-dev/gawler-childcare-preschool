7944c44a37059d9e1ae0bb292f612a2c8a20bd16
Dan <github@danp.dev>
2021-01-25 14:39:45 +1030

    initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,25 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
-    },  
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Gawler Community Childcare and Preschool",
+        short_name: "Gawler Childcare Preschool",
+        start_url: "/",
+        background_color: "#276CDA",
+        theme_color: "#276CDA",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
     `gatsby-plugin-offline`,
   ],
 }
