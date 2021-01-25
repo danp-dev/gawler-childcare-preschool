12ae6c609d93d983c95c5c12db6d1681b1336610
Dan <github@danp.dev>
2021-01-25 14:12:49 +1030

    initial

---
+++ src/components/booktour.js
@@ -0,0 +1,99 @@
+import React from "react"
+import { Link } from "gatsby"
+
+
+const Booktour = () => (
+    <section className="section">
+      <div className="columns is-mobile">
+      <div className="column is-half-desktop is-offset-one-quarter-desktop is-three-full-mobile">
+        <div className="container has-text-centered">
+          <h1 className="title is-size-2">Request A Tour</h1>
+          <h3 className="title is-size-4">We just need a little information to get you booked in.</h3>
+        </div>
+          
+      <form name="contact" method="POST" data-netlify="true">
+
+<div className="field">
+<label className="label">Your Name</label>
+<div className="control">
+<input className="input" type="text" placeholder="Text input"/>
+</div>
+</div>
+
+<div className="field">
+<label className="label">Your preferred date of Tour</label>
+<div className="control">
+<input className="input" type="date" placeholder="Text input"/>
+</div>
+</div>
+
+
+<div className="field">
+<label className="label">Email</label>
+<div className="control has-icons-left has-icons-right">
+<input className="input is-danger" type="email" placeholder="Email input"/>
+<span className="icon is-small is-left">
+<i className="fas fa-envelope"></i>
+</span>
+<span className="icon is-small is-right">
+<i className="fas fa-exclamation-triangle"></i>
+</span>
+</div>
+</div>
+
+<div className="field">
+<label className="label">Child's Age</label>
+<div className="control">
+<input className="input" type="text" placeholder="Text input"/> 
+</div>
+</div>
+
+<div className="field">
+<label className="label">Message</label>
+<div className="control">
+<input className="textarea" placeholder="Textarea"></input>
+</div>
+</div>
+
+<div className="field">
+<div className="control">
+<label className="checkbox">
+<input type="checkbox"/>
+I agree to the <a href="#">terms and conditions</a>
+</label>
+</div>
+</div>
+
+<div className="field">
+<div className="control">
+<label className="radio">
+<input type="radio" name="question"/>
+Yes
+</label>
+<label className="radio">
+<input type="radio" name="question"/>
+No
+</label>
+</div>
+</div>
+
+<div className="field is-grouped">
+<div className="control">
+<button className="button is-link">Submit</button>
+</div>
+<div className="control">
+<button className="button is-link is-light">Cancel</button>
+</div>
+</div>
+
+</form>
+
+
+
+      </div>
+      </div>
+    </section>
+
+)
+
+export default Booktour
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import backgroundimg from "../images/Gawler-Childcare-Header-Image.png"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import BookTour from "../components/booktour"
 
 import tempimage from "../images/gatsby-astronaut.png"
 
@@ -215,96 +216,9 @@ const IndexPage = ({data}) => (
 
     </section>
 
-    <section className="section">
-      <div className="columns is-mobile">
-      <div className="column is-three-fifths-desktop is-offset-one-fifth-desktop is-full-mobile">
-        <div className="container has-text-centered">
-          <h1 className="title is-size-2">Get In Touch</h1>
-          <h3 className="title is-size-4">Send Us A Message</h3>
-        </div>
-          
-      <form name="contact" method="POST" data-netlify="true">
-
-<div className="field">
-<label className="label">Name</label>
-<div className="control">
-<input className="input" type="text" placeholder="Text input"/>
-</div>
-</div>
-
-<div className="field">
-<label className="label">Email</label>
-<div className="control has-icons-left has-icons-right">
-<input className="input is-danger" type="email" placeholder="Email input"/>
-<span className="icon is-small is-left">
-<i className="fas fa-envelope"></i>
-</span>
-<span className="icon is-small is-right">
-<i className="fas fa-exclamation-triangle"></i>
-</span>
-</div>
-</div>
-
-<div className="field">
-<label className="label">Which Room are you interested in?</label>
-<div className="control">
-<div className="select">
-<select>
-  <option>Select</option>
-  <option>Nursery</option>
-  <option>Toddler</option>
-  <option>Kindy</option>
-  <option>Preschool</option>
-</select>
-</div>
-</div>
-</div>
-
-<div className="field">
-<label className="label">Message</label>
-<div className="control">
-<textarea className="textarea" placeholder="Textarea"></textarea>
-</div>
-</div>
-
-<div className="field">
-<div className="control">
-<label className="checkbox">
-<input type="checkbox"/>
-I agree to the <a href="#">terms and conditions</a>
-</label>
-</div>
-</div>
-
-<div className="field">
-<div className="control">
-<label className="radio">
-<input type="radio" name="question"/>
-Yes
-</label>
-<label className="radio">
-<input type="radio" name="question"/>
-No
-</label>
-</div>
-</div>
-
-<div className="field is-grouped">
-<div className="control">
-<button className="button is-link">Submit</button>
-</div>
-<div className="control">
-<button className="button is-link is-light">Cancel</button>
-</div>
-</div>
-
-</form>
+  <BookTour />  
 
 
-
-      </div>
-      </div>
-    </section>
   </Layout>
 )
 
