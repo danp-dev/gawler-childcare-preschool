33055e54264f6382501e3584097644d022aa0249
Dan <github@danp.dev>
2021-03-11 14:02:45 +1030

    initial

--- src/components/booktour.js
+++ src/components/booktour.js
@@ -9,46 +9,26 @@ const Booktour = () => {
    
 
     <section className="section">
-       <Helmet defer="false">
-    <script type='text/javascript'>
-      {`(function(f,o,r,m){
-        r=f.createElement('script');r.async=1;r.src=o+'js/mf.js';
-        m=f.getElementById('mf_placeholder'); m.parentNode.insertBefore(r, m);
-      })(document,'//upeuuyenjop.machform-trial.com/');`}
-    </script>
-        </Helmet>
         
       <div className="columns is-mobile">
       <div className="column is-half-desktop is-offset-one-quarter-desktop is-three-full-mobile">
-        <div id="mf_placeholder" 
-     data-formurl="//upeuuyenjop.machform-trial.com/embed.php?id=10695" 
-     data-formheight="553"  
-     data-formtitle="Untitled Form"  
-     data-paddingbottom="10">
-</div>
           
       <form name="contact" method="POST" data-netlify="true">
       <input type="hidden" name="form-name" value="contact"/>
   <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
+    <label className="label">Your Name: <input type="text" className="input" name="name" /></label>   
   </p>
   <p>
-    <label>Your Email: <input type="email" name="email" /></label>
+    <label className="label">Your Email: <input type="email" className="input" name="email" /></label>
   </p>
   <p>
-    <label>Your Preferred Date of Tour: <input type="date" name="date" /></label>
+    <label className="label">Your Preferred Date of Tour: <input type="date" className="input" name="date"/></label>
   </p>
   <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
+    <label className="label">Message: <textarea name="message" className="textarea"></textarea></label>
   </p>
   <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
+    <button type="submit" className="button">Book Tour</button>
   </p>
 </form>
 
